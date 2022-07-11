8afb2a18e275ee137c8086d1e7973785841f34c7
unknown <masoodg1998@gmail.com>
2022-07-11 17:11:09 +0500

    some code

--- index.js
+++ index.js
@@ -14,7 +14,47 @@ dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")))
 
-mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to MongoDB")).catch(err=>(console.log(err)));
+// mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to MongoDB")).catch(err=>(console.log(err)));
+
+
+
+let dbURI = process.env.MONGO_URL
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+///////////************** Mongodb connected or disconnected Events ***********/////////////
+
+mongoose.connection.on('connected', function () {
+    console.log("Mongoose is connected")
+
+})
+
+mongoose.connection.on('disconnectes', function () {
+    console.log("mongoose is disconnected")
+    process.exit(1)
+})
+
+
+mongoose.connection.on('error', function (err) {
+    console.log('mongoose connecion is in error: ', err)
+    process.exit(1)
+
+})
+
+mongoose.connection.on('SIGNIT', function () {
+    console.log('app is turminating')
+    mongoose.connection.close(function () {
+        console.log('mongoose default connection is closed')
+        process(0)
+    })
+
+
+})
+
+
+
+
+
+
 
 const storage = multer.diskStorage({
     destination : (req,file,cb)=>{
--- models/models.js
+++ models/models.js
@@ -1,35 +1,3 @@
 var mongoose = require('mongoose');
 
 
-
-let dbURI = process.env.MONGO_URL
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-///////////************** Mongodb connected or disconnected Events ***********/////////////
-
-mongoose.connection.on('connected', function () {
-    console.log("Mongoose is connected")
-
-})
-
-mongoose.connection.on('disconnectes', function () {
-    console.log("mongoose is disconnected")
-    process.exit(1)
-})
-
-
-mongoose.connection.on('error', function (err) {
-    console.log('mongoose connecion is in error: ', err)
-    process.exit(1)
-
-})
-
-mongoose.connection.on('SIGNIT', function () {
-    console.log('app is turminating')
-    mongoose.connection.close(function () {
-        console.log('mongoose default connection is closed')
-        process(0)
-    })
-
-
-})
