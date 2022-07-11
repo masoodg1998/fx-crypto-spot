0e0fc072eec5385b8eca5ac215095642ac5c4ce7
unknown <masoodg1998@gmail.com>
2022-07-11 17:06:22 +0500

    some code

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")))
 
-mongoose.connect(process.env.MONGO_URL,).then(console.log("Connected to MongoDB")).catch(err=>(console.log(err)));
+mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to MongoDB")).catch(err=>(console.log(err)));
 
 const storage = multer.diskStorage({
     destination : (req,file,cb)=>{
