dbbfb31fcae48ed02d605a988a5f4d877c96e97a
unknown <masoodg1998@gmail.com>
2022-07-11 20:13:11 +0500

    abc

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,16 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const Post = require("../models/Post");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt-inzi")
+
 
 // UPDATE
 router.put("/:id", async (req,res)=>{
     if(req.body.userId === req.params.id){
         if(req.body.password){
             const salt = await bcrypt.genSalt(10);
-            req.body.password = await bcrypt.hash(req.body.password, salt);
+            req.body.password = await bcrypt.stringToHash(req.body.password, salt);
         }
         try{
             const updatedUser = await User.findByIdAndUpdate(req.params.id,{
